16032ea67a47fa31bc4833084f864119173a501a
lyyiangang <lyyiangang@sina.com>
2018-01-18 20:01:41 +0800

    add some code

--- hsv_info.py
+++ hsv_info.py
@@ -1,46 +1,44 @@
 import numpy as np
 import cv2
 
-img_file = "seatBeltWhite.png"
+img_file = "test.png"
 original_img = cv2.imread(img_file)
 img = original_img.copy()
 ply_pts = []
 
-i_before = id(img)
-
-# def test(img):
-#     other_img  = np.array([2,3])
-#     np.copyto(other_img, img)
-#     i_a = id(img)
-#     a =0
-
-
-# test(img)
-# i_after = id(img)
-
 def plot(img, pts):
     global original_img
-    #img = original_img.copy()
-    np.copyto(original_img, img)
+    img = original_img.copy()
     for pt in pts:
         cv2.circle(img, center = pt, radius = 4, color = (0,0,255))
     npts = len(pts)
     if npts <2 :
-        return
+        return img
     for ii in range(npts):
         if ii == npts -1 :
             cv2.line(img, pts[ii], pts[0], color=(255,0,0))
         else:
             cv2.line(img, pts[ii], pts[ii+1], color = (255, 0, 0))
+    return img
 
+def dump_info(img, pts):
+    if len(pts) < 3:
+        return
+    hsv_img = None
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    cv2.imshow("hsv_img", hsv_img)
+    hsv =[]
+    
+    #cv2.pointPolygonTest(contour, pts)
 
 def on_mouse_cb(event, x, y, flags, userdata):
-    global img
+    global img, original_img, ply_pts
     if event == cv2.EVENT_LBUTTONDOWN :
         ply_pts.append((x,y))
+        dump_info(original_img, ply_pts)
     elif event == cv2.EVENT_RBUTTONDOWN:
         ply_pts.clear()
-    plot(img, ply_pts)
+    img = plot(img, ply_pts)
 
 while True:
     cv2.imshow("test", img)
