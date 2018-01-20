1eeefa3c5744748f60b063e5fbb35354a53de5c4
lyyiangang <lyyiangang@sina.com>
2018-01-20 19:03:58 +0800

    finish hsv_info.py

--- hsv_info.py
+++ hsv_info.py
@@ -10,7 +10,7 @@ def plot(img, pts):
     global original_img
     img = original_img.copy()
     for pt in pts:
-        cv2.circle(img, center = pt, radius = 4, color = (0,0,255))
+        cv2.circle(img, center = pt, radius = 2, color = (0,0,255))
     npts = len(pts)
     if npts <2 :
         return img
@@ -24,12 +24,19 @@ def plot(img, pts):
 def dump_info(img, pts):
     if len(pts) < 3:
         return
-    hsv_img = None
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     cv2.imshow("hsv_img", hsv_img)
-    hsv =[]
-    
-    #cv2.pointPolygonTest(contour, pts)
+    pts = np.array(pts, np.int)
+    min_hsv = [1.0e6, 1.0e6, 1.0e6]
+    max_hsv = [-1, -1, -1]
+    for ((x,y,z),val) in np.ndenumerate(hsv_img):
+        if cv2.pointPolygonTest(pts, (y, x), False) <= 0:
+            continue
+        min_hsv[z] = min_hsv[z] if min_hsv[z] < val else val
+        max_hsv[z] = max_hsv[z] if max_hsv[z] > val else val
+
+    print("min_vals:{}, max_vals:{}".format(min_hsv, max_hsv))
+
 
 def on_mouse_cb(event, x, y, flags, userdata):
     global img, original_img, ply_pts
@@ -39,6 +46,7 @@ def on_mouse_cb(event, x, y, flags, userdata):
     elif event == cv2.EVENT_RBUTTONDOWN:
         ply_pts.clear()
     img = plot(img, ply_pts)
+
 
 while True:
     cv2.imshow("test", img)
