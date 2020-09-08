3be8781188dc64b09591c4c67dca51b372c1aed2
yangyang li <yangyang.li@bst.ai>
2020-09-08 13:22:27 +0800

    remove comments

--- socket/ServerWrapper.py
+++ socket/ServerWrapper.py
@@ -1,20 +1,6 @@
 import socket
 import threading
 import time
-
-
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# # 监听端口:
-# s.bind(('127.0.0.1', 9999))
-# s.listen(5)
-# print('Waiting for connection...')
-# while True:
-#     # 接受一个新连接:
-#     sock, addr = s.accept()
-#     # 创建新线程来处理TCP连接:
-#     t = threading.Thread(target=tcplink, args = (sock, addr))
-#     t.start()
-
 class ServerWarpper:
     def __init__(self, ip : str, port : int, max_connection_num : int = 5):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -45,7 +31,8 @@ class ServerWarpper:
         print('Connection from %s:%s closed.' % addr) 
 
 if __name__ == '__main__':
-    s = ServerWarpper('127.0.0.1', 9999)
+    # s = ServerWarpper('127.0.0.1', 9999) # for local machine
+    s = ServerWarpper('0.0.0.0', 9999) # for LAN clinet
     for cmd in 'abcd':
         s.send_command(cmd)
         time.sleep(0.01)
